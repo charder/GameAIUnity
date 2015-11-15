770c58a417566d7e6c1b34eeeca9991249810771
charder <chardergaming@gmail.com>
2015-11-15 13:35:27 -0500

    Fixed StateCondition, added GhostDie.

---
+++ Assets/GhostDie.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostDie : StateCondition {
+	public bool checkCondition(GameObject thisobject)
+	{
+		return (Vector3.Distance(thisobject.transform.position,wizard.pacman.transform.position) < 1 && wizard.pacman.isSuper)
+	}
+}
