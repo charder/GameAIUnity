07a2336218ca297a2ddc714913a7c7a00114f3af
charder <chardergaming@gmail.com>
2015-11-17 20:59:15 -0500

    Clyde is still dumb, but now runs dumb.

--- Assets/BehavClydeEvade.cs
+++ Assets/BehavClydeEvade.cs
@@ -6,6 +6,31 @@ public class BehavClydeEvade : Behavior {
 	}
 
 	public override void performBehavior() {
-		//TODO: finish this function
+		int random_int = Mathf.FloorToInt(Random.Range (0, 4));
+		if (random_int == 0) { //move Clyde Up
+			if (wizard.world.boolTiles[(int) this.wizard.clyde.transform.position.y + 1,(int) this.wizard.clyde.transform.position.x])
+			{
+				wizard.clyde.transform.position = new Vector3(wizard.clyde.transform.position.x, this.wizard.clyde.transform.position.y + 1,-2);
+			}
+		}
+		else if (random_int == 1){ //move Clyde Right
+			if (wizard.world.boolTiles[(int) this.wizard.clyde.transform.position.y,(int) this.wizard.clyde.transform.position.x + 1])
+			{
+				wizard.clyde.transform.position = new Vector3(wizard.clyde.transform.position.x + 1, this.wizard.clyde.transform.position.y,-2);
+			}
+			
+		}
+		else if (random_int == 2){ //move Clyde Left
+			if (wizard.world.boolTiles[(int) this.wizard.clyde.transform.position.y - 1,(int) this.wizard.clyde.transform.position.x])
+			{
+				wizard.clyde.transform.position = new Vector3(wizard.clyde.transform.position.x, this.wizard.clyde.transform.position.y - 1,-2);
+			}
+		}
+		else if (random_int == 3){ //move Clyde Up
+			if (wizard.world.boolTiles[(int) this.wizard.clyde.transform.position.y,(int) this.wizard.clyde.transform.position.x - 1])
+			{
+				wizard.clyde.transform.position = new Vector3(wizard.clyde.transform.position.x - 1, this.wizard.clyde.transform.position.y,-2);
+			}
+		}
 	}
 }
