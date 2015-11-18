8a8c3f797d7d33c3636f6a5175750c9bf0a67778
goubew <goubew@users.noreply.github.com>
2015-11-17 21:37:12 -0500

    It's alive

--- Assets/BehavPacmanEat.cs
+++ Assets/BehavPacmanEat.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BehavPacmanEat : Behavior {
+	public LinkedList<Vector3> path;
+
 	public BehavPacmanEat() {
 		findWizard ();
 	}
 
 	public override void performBehavior() {
-		float dist = float.MaxValue;
-
-
+		if (path == null || path.Count <= 0) {
+			float dist = float.MaxValue;
+			int x = (int) wizard.pacman.transform.position.x;
+			int y = (int) wizard.pacman.transform.position.y;
+			
+			for (int i = 0; i < wizard.world.points.GetLength(0); i++) {
+				for (int j = 0; j < wizard.world.points.GetLength(1); j++) {
+					if (wizard.world.points[i, j] != null) {
+						float currentDist = Vector3.Distance(wizard.pacman.transform.position, new Vector3(j, i));
+						
+						if (currentDist < dist) {
+							dist = currentDist;
+							x = j;
+							y = i;
+						}
+					}
+				}
+			}
+			path = wizard.world.findPath (wizard.pacman.transform.position, new Vector3(x, y));
+		}
+		wizard.pacman.transform.position = new Vector3(path.First.Value.x, path.First.Value.y, -2);
+		path.RemoveFirst ();
 	}
 }
--- Assets/BehavPacmanPower.cs
+++ Assets/BehavPacmanPower.cs
@@ -1,11 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BehavPacmanPower : Behavior {
+	public LinkedList<Vector3> path;
+
 	public BehavPacmanPower() {
 		findWizard ();
 	}
 
 	public override void performBehavior() {
-		//TODO: finish this function
+		if (path == null || path.Count <= 0) {
+			string ghost = "";
+			float dist = float.MaxValue;
+			float currentDist;
+			
+			currentDist = Vector3.Distance (wizard.blinky.transform.position, wizard.pacman.transform.position);
+			if (currentDist < dist) {
+				dist = currentDist;
+				ghost = "B";
+			}
+			
+			currentDist = Vector3.Distance (wizard.pinky.transform.position, wizard.pacman.transform.position);
+			if (currentDist < dist) {
+				dist = currentDist;
+				ghost = "P";
+			}
+			
+			currentDist = Vector3.Distance (wizard.inky.transform.position, wizard.pacman.transform.position);
+			if (currentDist < dist) {
+				dist = currentDist;
+				ghost = "I";
+			}
+			
+			currentDist = Vector3.Distance (wizard.clyde.transform.position, wizard.pacman.transform.position);
+			if (currentDist < dist) {
+				dist = currentDist;
+				ghost = "C";
+			}
+			
+			if (ghost == "B") {
+				path = wizard.world.findPath(wizard.pacman.transform.position, wizard.blinky.transform.position);
+			} else if (ghost == "P") {
+				path = wizard.world.findPath(wizard.pacman.transform.position, wizard.pinky.transform.position);
+			} else if (ghost == "I") {
+				path = wizard.world.findPath(wizard.pacman.transform.position, wizard.inky.transform.position);
+			} else if (ghost == "C") {
+				path = wizard.world.findPath(wizard.pacman.transform.position, wizard.clyde.transform.position);
+			}
+		}
+		wizard.pacman.transform.position = new Vector3(path.First.Value.x, path.First.Value.y, -2);
+		path.RemoveFirst ();
 	}
 }
